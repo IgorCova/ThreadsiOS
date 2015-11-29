f9770143b07915e2ebf89908cb8d9f454515d9df
Igor Cova <admin@Mac-Admin.local>
2015-11-29 12:39:08 +0300

    Add load photo

--- Threads/MenuView.swift
+++ Threads/MenuView.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
-class MenuView: UIViewController {
+class MenuView: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
 
     @IBOutlet weak var lblPersonName: UILabel!
     @IBOutlet weak var imgPerson: UIImageView!
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,8 @@ class MenuView: UIViewController {
         self.imgPerson.layer.cornerRadius = self.imgPerson.frame.size.height/2
         self.imgPerson.layer.masksToBounds = true
         self.imgPerson.layer.borderWidth = 0.1
+        
+        //self.imgPerson.image = 
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,6 +34,29 @@ class MenuView: UIViewController {
     @IBAction func showCommunities() {
     }
 
+    @IBAction func setPhoto() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.SavedPhotosAlbum){
+            print("\nButton capture")
+            
+            self.imagePicker.delegate = self
+            self.imagePicker.sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum;
+            self.imagePicker.allowsEditing = false
+            
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+
+    }
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+        self.dismissViewControllerAnimated(true, completion: { () -> Void in })
+        self.imgPerson.image = image
+        
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        let newImage = NSEntityDescription.insertNewObjectForEntityForName("Person", inManagedObjectContext: context)
+        newImage.setValue(image.description, forKey: "photo")
+    }
     /*
     // MARK: - Navigation
 
